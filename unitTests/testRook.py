e03f63740a6c1e5d83747dbbe8e2bfcada693e2d
kovkir <kovaletskir@gmail.com>
2023-04-23 13:40:20 +0300

    Исправлено: unit тесты для Ладьи

--- unitTests/testRook.py
+++ unitTests/testRook.py
@@ -8,19 +8,44 @@ from chessPieces.rook import Rook
 
 class TestRook(unittest.TestCase):
     def setUp(self):
-        self.wRook = Rook(Canvas(), 100, 0, 100, "Rook", "white", 0, 7)
-        self.bRook = Rook(Canvas(), 100, 0, 100, "Rook", "black", 7, 0)
-        
+        self.wRook = Rook(Canvas(), 100, 0, 100, "Rook", "white", 0, 5)
+        self.bRook = Rook(Canvas(), 100, 0, 100, "Rook", "black", 7, 4)
+
+        self.wChessBool = [
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, True,  False, False],
+            [True,  False, False, True,  False, False, False, False],
+            [False, True,  True,  False, True,  False, True,  True ],
+            [False, True,  True,  True,  True,  True,  True,  True ],
+        ]
+        self.bChessBool = [
+            [True,  True,  True,  True,  True,  True,  True,  False],
+            [True,  True,  True,  True,  True,  True,  True,  False],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, True ],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, False],
+            [False, False, False, False, False, False, False, False],
+        ]
+    
     
     def testWRookMovement(self):
-        movement = [[0, 0], [0, 1], [1, 7], [0, 2], [2, 7], [0, 3], [3, 7], 
-                    [0, 4], [4, 7], [0, 5], [5, 7], [0, 6], [6, 7], [7, 7]]
-        
-        self.assertEqual(self.wRook.calculateMovement(True, True), movement)
+        correctMovements = [[0, 4], [0, 3], [0, 2], [0, 1], [1, 5], [2, 5], [0, 6], [0, 7]]
+
+        self.assertEqual(
+            self.wRook.calculateMovement(True, True, self.wChessBool, self.bChessBool),
+            correctMovements
+        )
 
     
     def testBRookMovement(self):
-        movement = [[0, 0], [7, 1], [1, 0], [7, 2], [2, 0], [7, 3], [3, 0],
-                    [7, 4], [4, 0], [7, 5], [5, 0], [7, 6], [6, 0], [7, 7]]
-        
-        self.assertEqual(self.bRook.calculateMovement(True, True), movement)
+        correctMovements = [[7, 3], [7, 2], [7, 1], [7, 0], [7, 5], [7, 6], [6, 4], [5, 4]]
+
+        self.assertEqual(
+            self.bRook.calculateMovement(True, False, self.wChessBool, self.bChessBool),
+            correctMovements
+        )
